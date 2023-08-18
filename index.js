419a400ddc1d3eeb43db6a46dc182d0911706b21
dQw4w9WgXcQ <22358065+dQw4w9WgXcQ@users.noreply.github.com>
2023-08-18 01:32:25 -0700

    asdf

--- index.js
+++ index.js
@@ -3,8 +3,11 @@ import notifier from "node-notifier";
 
 const url =
   "https://raw.githubusercontent.com/SimplifyJobs/Summer2024-Internships/dev/README.md";
+const offseasonUrl =
+  "https://raw.githubusercontent.com/SimplifyJobs/Summer2024-Internships/dev/README-Off-Season.md";
 
 let prevText = null;
+let prevTextOffseason = null;
 
 while (true) {
   let text;
@@ -18,22 +21,61 @@ while (true) {
 
   if (prevText == null) {
     prevText = text;
-    continue;
+  } else {
+    if (prevText == text) {
+      console.log("\x1b[32m", ".");
+    } else {
+      console.log("\x1b[31m", "@@@@@@@@@@@@@@@@@@@@@@@@");
+      console.log("\x1b[31m", "@@@@@@@@@@@@@@@@@@@@@@@@");
+      console.log("\x1b[31m", "@@@@@@@@@@@@@@@@@@@@@@@@");
+      console.log("\x1b[31m", "@@@@@@@@@@@@@@@@@@@@@@@@");
+      console.log("\x1b[31m", "@@@@@@@@@@@@@@@@@@@@@@@@");
+      console.log("SUMMER");
+      console.log("https://github.com/SimplifyJobs/Summer2024-Internships");
+
+      notifier.notify({
+        title: "NEW JOB!!",
+        message: "NEW JOB!! summer",
+      });
+      prevText = text;
+    }
   }
 
-  if (prevText == text) {
-    console.log("\x1b[32m", "...");
+  let offseasonText;
+  try {
+    offseasonText = await fetch(offseasonUrl).then((response) =>
+      response.text()
+    );
+  } catch (e) {
+    console.log(e);
     await sleep();
     continue;
   }
 
-  console.log("\x1b[31m", "change detected");
+  if (prevTextOffseason == null) {
+    prevTextOffseason = offseasonText;
+  } else {
+    if (prevTextOffseason == offseasonText) {
+      console.log("\x1b[32m", "..");
+    } else {
+      console.log("\x1b[31m", "@@@@@@@@@@@@@@@@@@@@@@@@");
+      console.log("\x1b[31m", "@@@@@@@@@@@@@@@@@@@@@@@@");
+      console.log("\x1b[31m", "@@@@@@@@@@@@@@@@@@@@@@@@");
+      console.log("\x1b[31m", "@@@@@@@@@@@@@@@@@@@@@@@@");
+      console.log("\x1b[31m", "@@@@@@@@@@@@@@@@@@@@@@@@");
+      console.log("OFFSEASON");
+      console.log(
+        "https://github.com/SimplifyJobs/Summer2024-Internships/blob/dev/README-Off-Season.md"
+      );
 
-  notifier.notify({
-    title: "NEW JOB!!",
-    message: "NEW JOB!!",
-  });
-  prevText = text;
+      notifier.notify({
+        title: "NEW JOB!!",
+        message: "NEW JOB!! offseason",
+      });
+      prevTextOffseason = offseasonText;
+    }
+  }
+
   await sleep();
 }
 
